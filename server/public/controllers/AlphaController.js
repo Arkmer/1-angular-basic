702138f39837846370c3c6dc780d27b191a37080
Ryan Udelhoven <Ryan@Udelhoven.net>
2018-08-11 19:44:24 -0500

    Input clears upon submit

--- server/public/controllers/AlphaController.js
+++ server/public/controllers/AlphaController.js
@@ -9,11 +9,11 @@ app.controller('AlphaController', ['AlphaService', '$scope', function(AlphaServi
     self.textGet = AlphaService.textGet;
     self.textDelete = AlphaService.textDelete;
     self.textEdit = AlphaService.textEdit;
+    self.textInput = AlphaService.textInput;
 
     self.textGet();
 
     self.toggleEdit = function(input, text, id){
         input.editHide = !input.editHide;
-        console.log('toggleEdit:', text, 'ID:', id);
     }
 }]);
--- server/public/services/alpha.service.js
+++ server/public/services/alpha.service.js
@@ -5,9 +5,11 @@ app.service('AlphaService', ['$http', function($http){
     self.textHold = {text: ''};
     self.textArray = [];
     self.update = {};
+    self.textInput = '';
 
     self.textCommit = function(textInput){
         self.textHold = {text: textInput};
+        self.textInput = '';
     };
 
     self.textPost = function(text){
@@ -17,7 +19,6 @@ app.service('AlphaService', ['$http', function($http){
             url: '/alpha',
             data: self.textHold
         }).then((response)=>{
-            console.log('textPost res:', response.data);
             self.textHold = {text: ''};
             self.textGet();
         }).catch((error)=>{
@@ -30,7 +31,6 @@ app.service('AlphaService', ['$http', function($http){
             methof: 'GET',
             url: '/alpha'
         }).then((response)=>{
-            console.log('textGet res.data:', response.data);
             self.textArray.list = response.data;
         }).catch((error)=>{
             console.log('textGet error:', error);
@@ -38,12 +38,10 @@ app.service('AlphaService', ['$http', function($http){
     }
 
     self.textDelete = function(id){
-        console.log('textDelete -- Start', id);
         $http({
             method: 'DELETE',
             url: `/alpha/${id}`
         }).then((response)=>{
-            console.log('textDelete res:', response.data);
             self.textGet();
         }).catch((error)=>{
             console.log('textDelete error:', error);
@@ -51,8 +49,6 @@ app.service('AlphaService', ['$http', function($http){
     }
 
     self.textEdit = function(edit, id){
-        console.log('textEdit -- Update:', edit);
-        console.log('textEdit -- ID:', id);
         self.update.edit = edit;
         self.update.id = id
         $http({
